1fc30d7d99a70cc3fe55582644ad1214c96b783e
JoleneLim22019027 <k22019027@kcl.ac.uk>
2023-10-02 15:35:44 +0100

    first commit

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    while (len(list) < number_of_primes):
+        i = 2
+        divFlag = True
+        while (divFlag and i < num):
+            if (num % i != 0):
+                i += 1
+                divFlag = True
+            else:
+                divFlag = False
+        
+        if divFlag:
+            list.append(num)
+        else:
+            num += 1
+
     return list
